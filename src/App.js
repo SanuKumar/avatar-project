d4f784c5fa8faded4d89cc60b23a259d4ad77b94
sanu_kumar <sanu.kumar@ptw.com>
2020-11-19 11:21:07 +0530

    fixed nav bar

--- src/App.js
+++ src/App.js
@@ -1,33 +1,53 @@
 import React, { Suspense, lazy } from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  useLocation,
-  Redirect,
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Navbar, Nav } from "react-bootstrap";
 import Login from "./components/Login/login";
 import { Loading } from "./components/Loading";
+import Todo from "./components/Todo/todo";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const Avatar = lazy(() => import("./components/Avatar/avatar"));
+
+const Header = () => {
+  return (
+    <Navbar bg="dark" variant="dark">
+      <Navbar.Brand href="/">
+        <FontAwesomeIcon icon={faCoffee} />
+      </Navbar.Brand>
+      <Nav className="mr-auto">
+        <Link to="/Avatar">Avatar</Link>
+        <Link to="/login">Login</Link>
+        <Link to="/todo">Todo</Link>
+      </Nav>
+    </Navbar>
+  );
+};
 
 const App = () => {
   return (
-    <Suspense
-      fallback={
-        <div>
-          <Loading
-            style={{
-              width: "100%",
-              height: "100vh",
-              background: "#fff",
-            }}
-          />
-        </div>
-      }
-    >
-      <Login />
-      <Avatar />
-    </Suspense>
+    <Router>
+      <Switch>
+        <Suspense
+          fallback={
+            <div>
+              <Loading
+                style={{
+                  width: "100%",
+                  height: "100vh",
+                  background: "#fff",
+                }}
+              />
+            </div>
+          }
+        >
+          <Header />
+          <Route path="/login" component={Login} />
+          <Route path="/avatar" component={Avatar} />
+          <Route path="/todo" component={Todo} />
+        </Suspense>
+      </Switch>
+    </Router>
   );
 };
 

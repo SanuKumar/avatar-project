2e27c942248ab0f541454a070a68b820962d4120
sanu_kumar <sanu.kumar@ptw.com>
2020-11-19 17:02:37 +0530

    fixed Navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React, { Suspense, lazy } from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink,
+} from "react-router-dom";
 import { Navbar, Nav } from "react-bootstrap";
 import Login from "./components/Login/login";
 import { Loading } from "./components/Loading";
@@ -9,16 +14,48 @@ import { faCoffee } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const Avatar = lazy(() => import("./components/Avatar/avatar"));
 
+// https://reactrouter.com/web/api/NavLink
+
 const Header = () => {
   return (
     <Navbar bg="dark" variant="dark">
       <Navbar.Brand href="/">
         <FontAwesomeIcon icon={faCoffee} />
       </Navbar.Brand>
-      <Nav className="mr-auto">
-        <Link to="/Avatar">Avatar</Link>
-        <Link to="/login">Login</Link>
-        <Link to="/todo">Todo</Link>
+      <Nav className="mr-auto" style={{ paddingLeft: "20px" }}>
+        <div style={{ paddingLeft: "20px" }}>
+          <NavLink
+            activeStyle={{
+              fontWeight: "bold",
+              color: "red",
+            }}
+            to="/Avatar"
+          >
+            Avatar
+          </NavLink>
+        </div>
+        <div style={{ paddingLeft: "20px" }}>
+          <NavLink
+            activeStyle={{
+              fontWeight: "bold",
+              color: "red",
+            }}
+            to="/login"
+          >
+            Login
+          </NavLink>
+        </div>
+        <div style={{ paddingLeft: "20px" }}>
+          <NavLink
+            activeStyle={{
+              fontWeight: "bold",
+              color: "red",
+            }}
+            to="/todo"
+          >
+            Todo
+          </NavLink>
+        </div>
       </Nav>
     </Navbar>
   );
